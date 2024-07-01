f89fb451b6b5138450acaaa1b00a15d1f6d04fbe
Nixon Azeredo <nixonazeredo@gmail.com>
2024-07-01 20:41:13 -0300

    Primeiro commit configuracao inicial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <>
+      <Button variant="contained">Hello world</Button>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+    </>
+  );
+}
+
+export default App;
