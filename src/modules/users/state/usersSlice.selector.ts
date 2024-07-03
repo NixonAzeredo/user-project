afa32c10869ea76c0be642dd11c30aede5bc41c2
Nixon Azeredo <39866741+NixonAzeredo@users.noreply.github.com>
2024-07-03 10:27:28 -0300

    adiciona console para utilizar state

--- src/modules/users/state/usersSlice.selector.ts
+++ src/modules/users/state/usersSlice.selector.ts
@@ -3,6 +3,9 @@ import { RootState } from "../../../core/store.interface";
 
 export const selectUserById = createSelector(
   (state: RootState) => state.users.entities,
-  (state: RootState, userId: number) => userId,
+  (state: RootState, userId: number) => {
+    console.log(state);
+    return userId;
+  },
   (entities, userId) => entities[userId]
 );
